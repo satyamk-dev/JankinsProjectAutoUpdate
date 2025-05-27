3706bb5a214a3453f6dfad2f3aaa36c011e46cfc
hp <hp@DESKTOP-SKH57DO.mshome.net>
2025-05-27 22:09:15 +0530

    change1

---
+++ DockerSBApp/src/main/java/com/nt/main/DockerSbAppApplication.java
@@ -0,0 +1,15 @@
+package com.nt.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSbAppApplication.class, args);
+	
+	System.out.println("😁Hello😎 I m Docker Comming from war file STS IDE");
+	}
+
+}
